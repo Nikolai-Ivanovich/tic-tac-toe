539caf4ec1e70ea23b763ecd2492614ade130801
Nikolai Ivanovich <seafarer1993@outlook.com>
2020-09-23 20:39:44 +0300

    Определены задачи

--- tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 #define SIZE 3
 
-void getAICoordinates(int xy[2]);
-void getUserCoordinates(int xy[2], char matrix[SIZE][SIZE]);
-
-void enterUserCoordinates(int xy[2]);
-bool isUserCorrectCoordinates(int xy[2]);
-bool isCoordinatesClear(int xy[2], char matrix[SIZE][SIZE]);
-
-void writeCoordinates(int xy[2], char matrix[SIZE][SIZE], char symbol);
-
-
 void printfMatrix(char matrix[SIZE][SIZE]);
 void getClearMatrix(char matrix[SIZE][SIZE]);
+void writeCoordinates(int xy[2], char matrix[SIZE][SIZE], char symbol);
+bool isCoordinatesClear(int xy[2], char matrix[SIZE][SIZE]);
+bool isWinnerDetermined(char matrix[SIZE][SIZE]);  // определение победителя и выход из бесконечного цикла
+// User
+void getUserCoordinates(int xy[2], char matrix[SIZE][SIZE]);
+void enterUserCoordinates(int xy[2]);
+bool isUserCorrectCoordinates(int xy[2]);
+
+// Ai
+void checkMatrixAttackAI(char matrix[SIZE][SIZE]);
+void checkMatrixDefAI(char matrix[SIZE][SIZE]);
+void searchClearCoordinatesAi(char matrix[SIZE][SIZE]);
+
+void getAICoordinates(int xy[2]);
+
+
+
+
+
 
 int main()
 {
@@ -34,6 +43,21 @@ int main()
     return 0;
 }
 
+void checkMatrixAttackAI(char matrix[SIZE][SIZE])
+{
+    // 1. должна проверить есть ли совпадения когда 2 ячейка O и одна свободна
+}
+
+void checkMatrixDefAI(char matrix[SIZE][SIZE])
+{
+    // 2. перекрытие выигрышных вариантов пользователя  когда 2 ячейка X и одна свободна
+}
+
+void searchClearCoordinatesAi(char matrix[SIZE][SIZE])
+{
+    // 3. Поиск свободных координат-ходов
+}
+
 void writeCoordinates(int xy[2], char matrix[SIZE][SIZE], char symbol)
 {
     matrix[xy[0]][xy[1]] = symbol;
@@ -48,8 +72,8 @@ bool isCoordinatesClear(int xy[2], char matrix[SIZE][SIZE])
 
 void getAICoordinates(int xy[2])
 {
-    xy[0] = rand() % 3;
-    xy[1] = rand() % 3;
+    xy[0] = rand() % SIZE;
+    xy[1] = rand() % SIZE;
 }
 
 void enterUserCoordinates(int xy[2])
