26c2861a0d1bc017b347ee41711d51a33b88e102
Nikolai Ivanovich <seafarer1993@outlook.com>
2020-09-24 00:08:44 +0300

    Добавлена рабочая функция определения победителя

--- tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+//#include <windows.h>
 using namespace std;
 #define SIZE 3
 
@@ -7,6 +8,13 @@ void getClearMatrix(char matrix[SIZE][SIZE]);
 void writeCoordinates(int xy[2], char matrix[SIZE][SIZE], char symbol);
 bool isCoordinatesClear(int xy[2], char matrix[SIZE][SIZE]);
 bool isWinnerDetermined(char matrix[SIZE][SIZE]);  // определение победителя и выход из бесконечного цикла
+bool isWinnerDeterminedHorisontal(char matrix[SIZE][SIZE]);
+bool isWinnerDeterminedVertical(char matrix[SIZE][SIZE]);
+bool isWinnerDeterminedSlantingLeftToRight(char matrix[SIZE][SIZE]);
+bool isWinnerDeterminedSlantingRightToLeft(char matrix[SIZE][SIZE]);
+
+void clearDisplay();
+
 // User
 void getUserCoordinates(int xy[2], char matrix[SIZE][SIZE]);
 void enterUserCoordinates(int xy[2]);
@@ -20,10 +28,6 @@ void searchClearCoordinatesAi(char matrix[SIZE][SIZE]);
 void getAICoordinates(int xy[2]);
 
 
-
-
-
-
 int main()
 {
     int xyAI[2], xyUser[2];
@@ -32,15 +36,126 @@ int main()
     getClearMatrix(matrix);
     printfMatrix(matrix);
 
-    getUserCoordinates(xyUser, matrix);
-    writeCoordinates(xyUser, matrix, 'x');
-    printfMatrix(matrix);
+
+    while (true)
+    {
+        
+        getUserCoordinates(xyUser, matrix);
+        writeCoordinates(xyUser, matrix, 'x');
+        clearDisplay();
+        printfMatrix(matrix);
+
+        if (isWinnerDetermined(matrix))
+        {
+            cout << "you win";
+            break;
+        }
+    }
+    clearDisplay();
+    cout << "you win";
 
        
 
     getAICoordinates(xyAI);
 
     return 0;
+}
+
+
+bool isWinnerDeterminedHorisontal(char matrix[SIZE][SIZE])
+{
+    int consistency = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 1; j < SIZE; j++)
+        {
+            if (matrix[i][0] == matrix[i][j] and matrix[i][0] != ' ')
+                consistency++;
+            else
+            {
+                break;
+            }
+        }
+        if (consistency == SIZE - 1)
+            return true;
+    }
+    return false;
+}
+
+bool isWinnerDeterminedVertical(char matrix[SIZE][SIZE])
+{
+    int consistency = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 1; j < SIZE; j++)
+        {
+            if (matrix[0][i] == matrix[j][i] and matrix[0][i] != ' ')
+                consistency++;
+            else
+            {
+                break;              
+            }
+        }
+        if (consistency == SIZE - 1)
+            return true;
+    }
+    return false;
+}
+
+bool isWinnerDeterminedSlantingLeftToRight(char matrix[SIZE][SIZE])
+{
+    int consistencyLeftToRight = 0;
+    for (int i = 1; i < SIZE; i++)
+    {
+        if (matrix[0][0] == matrix[i][i] and matrix[0][0] != ' ')
+            consistencyLeftToRight++;
+        else
+        {
+            break;
+        }
+    }
+    if (consistencyLeftToRight == SIZE - 1)
+    {
+        return true;
+    }
+    return false;
+}
+bool isWinnerDeterminedSlantingRightToLeft(char matrix[SIZE][SIZE])
+{
+    int consistencyRightToLeft = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (i + j == SIZE - 1)
+            {
+                if (matrix[0][SIZE - 1] == matrix[i][j] and matrix[i][j] != ' ')
+                    consistencyRightToLeft++;
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+    if (consistencyRightToLeft == SIZE)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isWinnerDetermined(char matrix[SIZE][SIZE])
+{
+    if (isWinnerDeterminedHorisontal(matrix))
+        return true;
+    else if (isWinnerDeterminedVertical(matrix))
+        return true;
+    else if (isWinnerDeterminedSlantingLeftToRight(matrix))
+        return true;
+    else if (isWinnerDeterminedSlantingRightToLeft(matrix))
+        return true;
+    return false;
 }
 
 void checkMatrixAttackAI(char matrix[SIZE][SIZE])
@@ -78,7 +193,7 @@ void getAICoordinates(int xy[2])
 
 void enterUserCoordinates(int xy[2])
 {
-    cout << "Please, enter you coordinates. X: [0;2], Y: [0,2]" << endl;
+    cout << endl << "Please, enter you coordinates. X: [0;2], Y: [0,2]" << endl;
     cout << "X: ";
     cin >> xy[0];
     cout << "Y: ";
@@ -139,4 +254,8 @@ void getClearMatrix(char matrix[SIZE][SIZE])
             matrix[i][j] = ' ';
         }
     }
+}
+
+void clearDisplay() {
+    system("cls");
 }
